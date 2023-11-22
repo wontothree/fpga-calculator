40b133335e31add2a8d99380d5c82401772485d9
williamkevin <prepared7913@naver.com>
2023-11-22 23:20:18 +0900

    -

--- 임시.v
+++ 임시.v
@@ -1,4 +1,27 @@
+module bin2bcd(
+   input [13:0] bin,
+   output reg [15:0] bcd
+   );
+   
+integer i;
+	
+always @(bin) begin
+    bcd=0;		 	
+    for (i=0;i<14;i=i+1) begin					//Iterate once for each bit in input number
+        if (bcd[3:0] >= 5) bcd[3:0] = bcd[3:0] + 3;		//If any BCD digit is >= 5, add three
+	if (bcd[7:4] >= 5) bcd[7:4] = bcd[7:4] + 3;
+	if (bcd[11:8] >= 5) bcd[11:8] = bcd[11:8] + 3;
+	if (bcd[15:12] >= 5) bcd[15:12] = bcd[15:12] + 3;
+	bcd = {bcd[14:0],bin[13-i]};				//Shift one bit, and shift in proper bit from input 
+    end
+end
+endmodule
+
 // reg [63:0] reg_rlt_bcd
+
+// 고쳐야 할 부분 1. 부호 표시
+// 고쳐야 할 부분 2. 숫자 앞에 있는 0을 blank로 바꾸기
+
 
 // lcd position passignment
 reg [8*16-1 : 0] reg_lcd_l1;
